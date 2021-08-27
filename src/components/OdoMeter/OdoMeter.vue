951a3e9936c36f9035c1070a554904a0d555aa33
Ebrahim ahmed <40615465+ebrahimahmed97090@users.noreply.github.com>
2021-08-27 21:22:22 +0200

    Fix re routing issue in odometer

--- src/components/OdoMeter/OdoMeter.vue
+++ src/components/OdoMeter/OdoMeter.vue
@@ -5,13 +5,21 @@
 </template>
 
 <script>
+import Odometer from "odometer/odometer";
+
 export default {
   name: "OdoMeter",
   mounted() {
-    setTimeout(function() {
-                 document.getElementById("odometer").innerHTML = 456;
-               },
-               1000);
+    let el = document.getElementById("odometer")
+    new Odometer({
+                            el   : el,
+                            value: 0,
+                            // Any option (other than auto and selector) can be passed in here
+                            format: "",
+                            theme : "default"
+                          });
+
+    el.innerHTML = 555
   }
 };
 </script>
